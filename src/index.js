7c0c079454454bfda39440e5d0f071dc1525a883
KateHickey26 <kate@tray.io>
2022-02-19 00:14:50 +0000

    update app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-import questionAPI from './question';
+import questionAPI from './question/EasyQbank';
 import QuestionBox from './components/QuestionBox';
 import Result from './components/ResultBox';
 
